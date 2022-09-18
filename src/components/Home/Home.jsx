7a83b4175878cb4318986caf401d83dc9a2547d3
Your User Name <fardoushjahanara@gmail.com>
2022-09-19 00:20:35 +0600

    banner create and left side

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="banner-wrapper">
+      <div className="banner-content-inner">
+      <h1 className="name">Hi...! I'm 
+      <br />
+      <span>Jahanara Fardoush</span></h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem inventore, laboriosam veritatis voluptatibus quam aspernatur officiis aut ex vitae consequuntur.</p>
+
+      <button className="home-btn btn">HIre Me</button>
+
+      <div className="banner-icon">
+        
+      </div>
+      </div>
+      <div className="home-right">
+<h2>Right</h2>
+      </div>
+    </div>
   )
 }
 
