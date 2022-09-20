ab7e6e101e0e870c122f965dc8e4f1ed856bb15a
Your User Name <fardoushjahanara@gmail.com>
2022-09-20 13:15:41 +0600

    install react icon and add images

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Home.css';
+import { FaFacebookF,FaInstagram,FaLinkedinIn,FaTwitter,FaGithub } from "react-icons/fa";
+import bannerImage from '../../images/banner1.png';
+
 
 const Home = () => {
   return (
@@ -12,12 +15,29 @@ const Home = () => {
 
       <button className="home-btn btn">HIre Me</button>
 
-      <div className="banner-icon">
-        
+      <div className="social-icon">
+        <a href="#gh" className=""> 
+        <FaFacebookF/>
+         </a>
+         <a href="#gh" className=""> 
+        <FaInstagram/>
+         </a>
+         <a href="#ghj" className=""> 
+        <FaLinkedinIn/>
+         </a>
+         <a href="#ghh" className=""> 
+        <FaTwitter/>
+         </a>
+         <a href="#" className=""> 
+        <FaGithub/>
+         </a>
       </div>
       </div>
-      <div className="home-right">
-<h2>Right</h2>
+      <div className="banner-images">
+      <span className='shape1'></span>
+      <img src={bannerImage} alt="" />
+      <img src={bannerImage} alt="" />
+
       </div>
     </div>
   )
