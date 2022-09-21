dcd1b00808d2a0197528280032b4dd7863a9c4f8
Your User Name <fardoushjahanara@gmail.com>
2022-09-22 00:35:57 +0600

    create services page and add to services function

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Services.css';
+
+
+const Services = () => {
+  return (
+    <div className='Services-wrapper'>
+      
+    </div>
+  )
+}
+
+export default Services;
