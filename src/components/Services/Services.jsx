4c61993798350663db9d00271052578c4e751cf0
Your User Name <fardoushjahanara@gmail.com>
2022-09-22 01:23:47 +0600

    add to Services content

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,8 +4,23 @@ import './Services.css';
 
 const Services = () => {
   return (
-    <div className='Services-wrapper'>
-      
+    <div className='services-wrapper'>
+      <div className="services-content">
+        <h2>My Awesome</h2>
+        <h2><span>Services</span></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti odit animi quis maxime sint nesciunt ducimus voluptas repellendus?</p>
+
+        <div className="downloadCv">
+            <button className="btn cv-btn">
+                Download CV
+            </button>
+        </div>
+        <div className="services-blur1"></div>  
+      </div>
+
+      <div className="services-main-content-inner">
+
+      </div>
     </div>
   )
 }
